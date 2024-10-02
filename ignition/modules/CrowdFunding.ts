91a694584e02988419aec739b6750c0c006beafa
rhammy <afaraeturahman@gmail.com>
2024-10-02 13:45:38 +0100

    Update config

--- ignition/modules/CrowdFunding.ts
+++ ignition/modules/CrowdFunding.ts
@@ -1,7 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const CrowdFundingModule = buildModule("CrowdFundingModule", (m) => {
-  const crowdFund = m.contract("CrowdFunding");
+  const crowdFund = m.contract("Crowdfunding");
 
   return { crowdFund };
 });
